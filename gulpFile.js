01688c07d4b708c13313ee166edd9311c5525904
Jerry Orta <jerryorta.dev@gmail.com>
2016-10-17 06:51:39 -0500

    feat(docs): conventianal change log

--- gulpFile.js
+++ gulpFile.js
@@ -38,6 +38,23 @@ gulp.task('instrumenter', function () {
     .pipe(gulp.dest('src/files/'));
 });
 
+// Update the changelog with the latest changes
+gulp.task('changelog', () => {
+  const conventionalChangelog = require('gulp-conventional-changelog');
+
+  return gulp.src('CHANGELOG.md')
+    .pipe(conventionalChangelog({
+      preset: 'angular',
+      releaseCount: 1
+    }, {
+      // Conventional Changelog Context
+      // We have to manually set version number so it doesn't get prefixed with `v`
+      // See https://github.com/conventional-changelog/conventional-changelog-core/issues/10
+      currentTag: require('./package.json').version
+    }))
+    .pipe(gulp.dest('./'));
+});
+
 gulp.task('bump.major', function(){
   gulp.src('./package.json')
     .pipe(bump({type:'major'}))
